c0a9404a27a48366d177b6a056a7fbaa0198216a
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-09-01 21:26:32 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -4,7 +4,7 @@ export const PhotoFrame = ({title,url}) => {
    // if(!title) return;
   return(
     <div className="photoframe">
-      <img src={url} alt="oops"/>
+      <img src={url}/>
       <div className="caption">{title}</div>
     </div>
   )
